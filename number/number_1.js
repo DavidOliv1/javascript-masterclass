d847ca4c71d0818704f6ad01399b809dda7e2121
DavidOliv1 <davidstna21@gmail.com>
2024-03-20 01:24:33 -0300

    adding examples from lecture about numbers

---
+++ number/number_1.js
@@ -0,0 +1,22 @@
+// O tipo Number é primitivo, imutável e é representado internamente pelo padrão IEEE 754 de 64 bits, ou seja, os numbers que vemos visualmente são internamente (em memória) convertidos para um binário de 64 bits (8 bytes).
+
+// O JavaScript suporta os sistemas de numeração decimal, hexadecimal, binário e octal
+
+// O sistema de numeração decimal, de base 10, deve iniciar com um número de 1 a 9, seguido por números de 0 a 9 com ou sem ponto, indicando se é inteiro ou decimal
+
+// O sistema de numeração hexadecimal, de base 16, deve iniciar com 0x ou 0X, seguido por números de 0 a 9 e letras de A a F
+
+// O sistema de numeração binário, de base 2, deve iniciar com 0b ou 0B, seguido por números de 0 a 1
+
+// O sistema de numeração octal, de base 8, deve iniciar com 0, 0o ou 0O seguido por números de 0 a 7
+
+// Por padrão, ao imprimirmos um number, é feito o toString e convertido para a base 10.
+
+console.log(10);
+console.log(9.9);
+console.log(0xff);
+console.log(0b11);
+console.log(010);
+console.log(0x1a);
+console.log(0o7);
+console.log(0o12);
